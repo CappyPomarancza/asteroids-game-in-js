19597ba28863aba0cf4e9cca454159360016af02
Kacper Swietlaga <kacperres@gmail.com>
2018-11-15 14:02:08 +0100

    solved problem with bullet

--- Asteroids/js/Bollet.js
+++ Asteroids/js/Bollet.js
@@ -11,8 +11,9 @@ Bullet.life = 35;
 
 function Bullet() {
     if (Bullet.active_count < Bullet.max) {
-        Bullet.count++;
         Bullet.active_count++;
+        Bullet.count++;
+
 
         this.id = Bullet.count.toString();
         Bullet.all[this.id] = this;
@@ -23,27 +24,37 @@ function Bullet() {
         this.x = Game.ship.points[0].x
         this.y = Game.ship.points[0].y
 
-        this.modX = Math.sin(Math.PI / 180 * this.a) * Bullet.speed * VAR.d
-        this.modY = -Math.cos(Math.PI / 180 * this.a) * Bullet.speed * VAR.d
+
     }
 }
 
 Bullet.draw = function () {
     for (let b in Bullet.all) {
-        if (Bullet.all[b].life < Bullet.life) {
-            Bullet.life ++
-            Bullet.all[b].x += Bullet.all[b].modX
-            Bullet.all[b].y += Bullet.all[b].modY
+        if (Bullet.all[b].life > Bullet.life) {
+            Bullet.active_count--
+            delete Bullet.all[b]
+        } else {
+            Bullet.all[b].life++
+            Bullet.all[b].x += Math.sin(Math.PI / 180 * Bullet.all[b].a) * Bullet.speed * VAR.d
+            Bullet.all[b].y -= Math.cos(Math.PI / 180 * Bullet.all[b].a) * Bullet.speed * VAR.d
 
+            if (Bullet.all[b].x < 0) {
+                Bullet.all[b].x += VAR.W
+            } else if (Bullet.all[b].x > VAR.W) {
+                Bullet.all[b].x -= VAR.W
+            }
+            if (Bullet.all[b].y < 0) {
+                Bullet.all[b].y += VAR.H
+            } else if (Bullet.all[b].y > VAR.H) {
+                Bullet.all[b].y -= VAR.H
+            }
+            //render bullet
             Game.ctx.beginPath()
             Game.ctx.arc(Bullet.all[b].x, Bullet.all[b].y, 3, 0, Math.PI / 180 * 360)
             Game.ctx.closePath()
             Game.ctx.fill()
-
-        }else{
-            Bullet.active_count--
-            delete Bullet.all[b]
-            
         }
     }
 }
+
+
