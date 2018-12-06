423722aef6c85976f7a617eb4ccb83173d4d4805
Kacper Swietlaga <kacperres@gmail.com>
2018-12-06 06:10:51 +0100

    added ship hit test

--- Asteroids/js/Ship.js
+++ Asteroids/js/Ship.js
@@ -20,75 +20,90 @@ function Ship() {
 	//
 	this.points = [{}, {}, {}];
 }
+Ship.prototype.hitTest = function () {
+	for (let i = 0; i < this.points.length; i++) {
+		for (let r in Rock.all) {
+			if (Rock.all[r].hitTest(this.points[i].x, this.points[i].y)) {
+				Rock.all[r].remove()
+				return true
+			}
+		}
+	}
+	return false
+}
 //
 Ship.prototype.draw = function () {
-	if (Game.key_37 || Game.key_39) {
-		this.a = this.a + 7 * (Game.key_37 ? -1 : 1);
-	}
-	if (Game.key_38) {
-		this.modX = Math.max(-this.maxMod*VAR.d, Math.min(this.maxMod*VAR.d, this.modX+Math.sin(Math.PI/180*this.a)*this.acc*VAR.d))
-		this.modY = Math.max(-this.maxMod*VAR.d, Math.min(this.maxMod*VAR.d, this.modY-Math.cos(Math.PI/180*this.a)*this.acc*VAR.d))
-	} else {
-		this.modX = this.modX * 0.98
-
-		this.modX = Math.abs(this.modX) < 0.0001 ? 0 : this.modX
-		this.modY = Math.abs(this.modY) < 0.0001 ? 0 : this.modY
-
-		this.modY = this.modY * 0.98
-	}
-	this.x += this.modX
-	this.y += this.modY
-
-	Game.ctx.beginPath()
-	// 
-	for (var i = 0; i < 3; i++) {
-		// przypisanie aktualnego kąta (różne w zależności od rysowanego punktu)
-		// dziób ma 180 stopni (i==0), rufa 50 (i==1) i -50 (i==2)
-		this.tmp_a = i === 0 ? this.a : (this.a + 180 + (i == 1 ? this.rear_a : -this.rear_a));
-		// aby przesunąć punkt obrotu został zmieniony promień okręgu dla rufy
-		// dziub (i==0) promień = r startku, rufa (i==1 albo i==2) promień = 60% r statku
-		this.tmp_r = i === 0 ? this.r * 1 : this.r * 0.6;
-		// 
-		// Punkty są przechowywane w tablicy obiektów.
-		//
-		this.points[i].x = (Math.sin(Math.PI / 180 * this.tmp_a) * this.tmp_r * VAR.d) + this.x;
-		this.points[i].y = (-Math.cos(Math.PI / 180 * this.tmp_a) * this.tmp_r * VAR.d) + this.y;
-		// Rysowanie
-		// (nie rysujemy tylko jak i==0)
-		Game.ctx[i === 0 ? 'moveTo' : 'lineTo'](this.points[i].x, this.points[i].y);
-	}
-	//
-	Game.ctx.closePath()
-	Game.ctx.stroke()
-
-
-	if (Game.key_38 && this.draw_thrust) {
-		Game.ctx.beginPath();
-		this.draw_thrust = false
-		for (let i = 0; i < 3; i++) {
-			this.tmp_a = i != 1 ? this.a + 180 + (i === 0 ? -this.rear_a + 14 : this.rear_a - 14) : this.a + 180
-			this.tmp_r = i == 1 ? this.r : this.r * 0.5
-			Game.ctx[i === 0 ? 'moveTo' : 'lineTo'](
-				Math.sin(Math.PI / 180 * this.tmp_a) * this.tmp_r * VAR.d + this.x,
-				-Math.cos(Math.PI / 180 * this.tmp_a) * this.tmp_r * VAR.d + this.y
-			);
+	if (!this.destroyed) {
+		if (this.hitTest()) {
+			this.destroyed = true
+			Game.stop()
 		}
+		if (Game.key_37 || Game.key_39) {
+			this.a = this.a + 7 * (Game.key_37 ? -1 : 1);
+		}
+		if (Game.key_38) {
+			this.modX = Math.max(-this.maxMod * VAR.d, Math.min(this.maxMod * VAR.d, this.modX + Math.sin(Math.PI / 180 * this.a) * this.acc * VAR.d))
+			this.modY = Math.max(-this.maxMod * VAR.d, Math.min(this.maxMod * VAR.d, this.modY - Math.cos(Math.PI / 180 * this.a) * this.acc * VAR.d))
+		} else {
+			this.modX = this.modX * 0.98
+
+			this.modX = Math.abs(this.modX) < 0.0001 ? 0 : this.modX
+			this.modY = Math.abs(this.modY) < 0.0001 ? 0 : this.modY
+
+			this.modY = this.modY * 0.98
+		}
+		this.x += this.modX
+		this.y += this.modY
+
+		Game.ctx.beginPath()
+		// 
+		for (var i = 0; i < 3; i++) {
+			// przypisanie aktualnego kąta (różne w zależności od rysowanego punktu)
+			// dziób ma 180 stopni (i==0), rufa 50 (i==1) i -50 (i==2)
+			this.tmp_a = i === 0 ? this.a : (this.a + 180 + (i == 1 ? this.rear_a : -this.rear_a));
+			// aby przesunąć punkt obrotu został zmieniony promień okręgu dla rufy
+			// dziub (i==0) promień = r startku, rufa (i==1 albo i==2) promień = 60% r statku
+			this.tmp_r = i === 0 ? this.r * 1 : this.r * 0.6;
+			// 
+			// Punkty są przechowywane w tablicy obiektów.
+			//
+			this.points[i].x = (Math.sin(Math.PI / 180 * this.tmp_a) * this.tmp_r * VAR.d) + this.x;
+			this.points[i].y = (-Math.cos(Math.PI / 180 * this.tmp_a) * this.tmp_r * VAR.d) + this.y;
+			// Rysowanie
+			// (nie rysujemy tylko jak i==0)
+			Game.ctx[i === 0 ? 'moveTo' : 'lineTo'](this.points[i].x, this.points[i].y);
+		}
+		//
+		Game.ctx.closePath()
 		Game.ctx.stroke()
-	} else if (Game.key_38 && !this.draw_thrust) {
-		this.draw_thrust = true
-	}
-
-	if (this.points[0].x < 0 && this.points[1].x < 0 && this.points[2].x < 0) {
-		this.x += VAR.W - Math.min(this.points[0].x, this.points[1].x, this.points[2].x) * 0.9
-	} else if (this.points[0].x > VAR.W && this.points[1].x > VAR.W && this.points[2].x > VAR.W) {
-		this.x -= VAR.W - (VAR.W - Math.max(this.points[0].x, this.points[1].x, this.points[2].x)) * 0.9
-	}
-	if (this.points[0].y < 0 && this.points[1].y < 0 && this.points[2].y < 0) {
-		this.y += VAR.H - Math.min(this.points[0].y, this.points[1].y, this.points[2].y) * 0.9
-	} else if (this.points[0].y > VAR.H && this.points[1].y > VAR.H && this.points[2].y > VAR.H) {
-		this.y -= VAR.H - (VAR.H - Math.max(this.points[0].y, this.points[1].y, this.points[2].y)) * 0.9
-	}
 
 
+		if (Game.key_38 && this.draw_thrust) {
+			Game.ctx.beginPath();
+			this.draw_thrust = false
+			for (let i = 0; i < 3; i++) {
+				this.tmp_a = i != 1 ? this.a + 180 + (i === 0 ? -this.rear_a + 14 : this.rear_a - 14) : this.a + 180
+				this.tmp_r = i == 1 ? this.r : this.r * 0.5
+				Game.ctx[i === 0 ? 'moveTo' : 'lineTo'](
+					Math.sin(Math.PI / 180 * this.tmp_a) * this.tmp_r * VAR.d + this.x,
+					-Math.cos(Math.PI / 180 * this.tmp_a) * this.tmp_r * VAR.d + this.y
+				);
+			}
+			Game.ctx.stroke()
+		} else if (Game.key_38 && !this.draw_thrust) {
+			this.draw_thrust = true
+		}
+
+		if (this.points[0].x < 0 && this.points[1].x < 0 && this.points[2].x < 0) {
+			this.x += VAR.W - Math.min(this.points[0].x, this.points[1].x, this.points[2].x) * 0.9
+		} else if (this.points[0].x > VAR.W && this.points[1].x > VAR.W && this.points[2].x > VAR.W) {
+			this.x -= VAR.W - (VAR.W - Math.max(this.points[0].x, this.points[1].x, this.points[2].x)) * 0.9
+		}
+		if (this.points[0].y < 0 && this.points[1].y < 0 && this.points[2].y < 0) {
+			this.y += VAR.H - Math.min(this.points[0].y, this.points[1].y, this.points[2].y) * 0.9
+		} else if (this.points[0].y > VAR.H && this.points[1].y > VAR.H && this.points[2].y > VAR.H) {
+			this.y -= VAR.H - (VAR.H - Math.max(this.points[0].y, this.points[1].y, this.points[2].y)) * 0.9
+		}
+	}
 
 };
--- Asteroids/js/game.js
+++ Asteroids/js/game.js
@@ -1,6 +1,6 @@
 // Inicjuję grę dopiero po załadowaniu całej strony
 window.onload = function () {
-	Game.init();
+	Game.init()
 }
 // Obiekt, w którym będą przechowywane „podręczne” wartości
 VAR = {
@@ -10,7 +10,7 @@ VAR = {
 	lastTime: 0,
 	lastUpdate: -1,
 	rand: function (min, max) {
-		return Math.floor(Math.random() * (max - min + 1)) + min;
+		return Math.floor(Math.random() * (max - min + 1)) + min
 	}
 }
 // Obiekt zawierający bazowe funckje związane z grą.
@@ -19,27 +19,31 @@ Game = {
 	// init zostanie odpalone raz po załadowaniu strony.
 	init: function () {
 		// 
-		Game.canvas = document.createElement('canvas');
+		Game.canvas = document.createElement('canvas')
 		Game.hit_canvas = document.createElement('canvas')
 		// 
 		Game.ctx = Game.canvas.getContext('2d')
 		this.hit_ctx = this.hit_canvas.getContext('2d')
 		//
-		Game.layout();
+		Game.layout()
 		// metoda layout odpali się przy każdej zmianie wielkości okna
-		window.addEventListener('resize', Game.layout, false);
+		window.addEventListener('resize', Game.layout, false)
 		//
-		document.body.appendChild(Game.canvas);
+		document.body.appendChild(Game.canvas)
 		//
 		Game.ship = new Ship();
 		for (let i = 0; i < 3; i++) {
 			new Rock()
 		}
-		window.addEventListener('keydown', Game.onKey, false);
-		window.addEventListener('keyup', Game.onKey, false);
+		window.addEventListener('keydown', Game.onKey, false)
+		window.addEventListener('keyup', Game.onKey, false)
 
-		// rozpoczęcie pętli gry
-		Game.animationLoop();
+		//
+		Game.animationLoop()
+	},
+	stop: function () {
+		window.removeEventListener('keydown', Game.onKey)
+		window.removeEventListener('keyup', Game.onKey)
 	},
 	onKey: function (event) {
 		if (event.keyCode == 37 || event.keyCode == 38 || event.keyCode == 39 || event.keyCode == 40 || event.keyCode == 32) {
@@ -55,7 +59,7 @@ Game = {
 					new Bullet()
 				}
 			} else if (event.type == 'keyup') {
-				Game['key_' + event.keyCode] = false;
+				Game['key_' + event.keyCode] = false
 			}
 		}
 
@@ -63,16 +67,16 @@ Game = {
 	// Ta metoda będzie odpalana przy każdej zmianie wielkości okna
 	layout: function (ev) {
 		// 
-		VAR.W = window.innerWidth;
-		VAR.H = window.innerHeight;
+		VAR.W = window.innerWidth
+		VAR.H = window.innerHeight
 		// 
-		VAR.d = Math.min(VAR.W, VAR.H);
+		VAR.d = Math.min(VAR.W, VAR.H)
 		// Update wielkości canvas
-		Game.canvas.width = VAR.W;
-		Game.canvas.height = VAR.H;
+		Game.canvas.width = VAR.W
+		Game.canvas.height = VAR.H
 		//
-		Game.hit_canvas.width = VAR.W;
-		Game.hit_canvas.height = VAR.H;
+		Game.hit_canvas.width = VAR.W
+		Game.hit_canvas.height = VAR.H
 		Game.hit_ctx.fillStyle = 'red'
 		//
 		Game.ctx.fillStyle = 'white'
@@ -81,20 +85,20 @@ Game = {
 		Game.ctx.lineJoin = 'round'
 		//
 		Game.hit_canvas.width = VAR.W
-		Game.hit_canvas.height = VAR.H 
+		Game.hit_canvas.height = VAR.H
 
 		Game.hit_ctx.fillStyle = '#ff0000'
 	},
 	// fps = 60
 	animationLoop: function (time) {
-		requestAnimationFrame(Game.animationLoop);
+		requestAnimationFrame(Game.animationLoop)
 		// ograniczenie do ilości klatek zdefiniowanych w właściwości obiektu VAR 
 		if (time - VAR.lastTime >= 1000 / VAR.fps) {
-			VAR.lastTime = time;
+			VAR.lastTime = time
 			//
 			Game.ctx.clearRect(0, 0, VAR.W, VAR.H)
 			// 
-			Game.ship.draw();
+			Game.ship.draw()
 			Bullet.draw()
 			Rock.draw()
 		}
